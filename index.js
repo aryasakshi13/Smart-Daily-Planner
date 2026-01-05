2375945b4e56f8e0a3cc5c37f7364f534dffef2b
Sakshi Arya <umangarya222@gmail.com>
2026-01-05 19:33:42 +0530

    Done with Css and LocalStorage adding media Query

--- index.js
+++ index.js
@@ -3,32 +3,34 @@
 //                 document.getElememtById('Conditonal-row').style.display='table-row'
 //             }
 
-            let task = [];
-           
+let task = JSON.parse(localStorage.getItem("tasks")) || [];
 
-            function addTask(){     
-                console.log("clciked")   
 
-                let taskDetails ={
-                    taskName : document.getElementById("taskName1").value,
-                    priority : document.getElementById("priority1").value,
-                    energy : document.getElementById("energy1").value,
-                    status : "Pending",
-                    completed: false
-                };
-                 task.push(taskDetails);
-                 showSortedTask();
-            }   
-            
-            function showSortedTask(){
-                let table = document.getElementById("Prioritytable");
-                let table2 = document.getElementById("completedTable");
-                table.innerHTML = "";
-                table2.innerHTML = "";
-                 Sortedtask();
-                 let seq=1;
-                task.forEach((t,index) => {
-                    let row = `
+function addTask() {
+    console.log("clciked")
+
+    let taskDetails = {
+        taskName: document.getElementById("taskName1").value,
+        priority: document.getElementById("priority1").value,
+        energy: document.getElementById("energy1").value,
+        status: "Pending",
+        completed: false
+    };
+    task.push(taskDetails);
+    localStorage.setItem("tasks", JSON.stringify(task));
+    showSortedTask();
+}
+
+function showSortedTask() {
+    let table = document.getElementById("Prioritytable");
+    let table2 = document.getElementById("completedTable");
+    table.innerHTML = "";
+    table2.innerHTML = "";
+    Sortedtask();
+    let seq = 1;
+    console.log(localStorage.getItem("task"));
+    task.forEach((t, index) => {
+        let row = `
                         <tr>
                         <td>${seq++}</td>
                         <td>${t.taskName}</td>
@@ -44,46 +46,47 @@
                         </tr>
 
                     `
-                    t.completed? table2.innerHTML += row: table.innerHTML += row;
-                    
-                    
-                });
-                 
-            }
-           
-            function toggleStatus(index) {
-                if (task[index].status === "Pending") {
-                    task[index].status = "Completed";
-                    task[index].completed=true;
-                }
-                showSortedTask();
-            }
+        t.completed ? table2.innerHTML += row : table.innerHTML += row;
 
-                function Sortedtask(){
-                    for(let i = 0;i<task.length;i++){
-                        for(let j = i+1;j<task.length;j++){
-                            if(!task[i].completed){
-                                if(task[i].priority == task[j].priority){
-                                    if(task[i].energy > task[j].energy){
-                                        let temp = task[i];
-                                        task[i] = task[j];
-                                        task[j] = temp;
-                                    }
-                                }else{
-                                    if(task[i].priority > task[j].priority){
-                                        let temp = task[i];
-                                        task[i] = task[j];
-                                        task[j] = temp;
-                                    }
-                                }
-                            }
-                        }
+
+    });
+
+}
+
+function toggleStatus(index) {
+    if (task[index].status === "Pending") {
+        task[index].status = "Completed";
+        task[index].completed = true;
+    }
+    localStorage.setItem("tasks", JSON.stringify(task));
+    showSortedTask();
+}
+
+function Sortedtask() {
+    for (let i = 0; i < task.length; i++) {
+        for (let j = i + 1; j < task.length; j++) {
+            if (!task[i].completed) {
+                if (task[i].priority == task[j].priority) {
+                    if (task[i].energy > task[j].energy) {
+                        let temp = task[i];
+                        task[i] = task[j];
+                        task[j] = temp;
                     }
-                    return task;
+                } else {
+                    if (task[i].priority > task[j].priority) {
+                        let temp = task[i];
+                        task[i] = task[j];
+                        task[j] = temp;
+                    }
                 }
+            }
+        }
+    }
+    return task;
+}
 
 
-                 
 
 
-            
+
+
